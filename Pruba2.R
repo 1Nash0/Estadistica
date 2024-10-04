08377deca5c4fdab0bc7524c309b3c2124a65537
César Alburquerque López <c.alburquerque.2023@alumnos.urjc.es>
2024-10-04 16:38:12 +0200

    Variables y grafica mal hecha

--- Pruba2.R
+++ Pruba2.R
@@ -1,1 +1,40 @@
-//Holaaa
+# Autor: Grupo 12
+
+# Carga datos ----
+library(dplyr)
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+lab <- read_csv("~/ESTADISTICA/Practica Grupal/AnalisisAIRBNB/Airbnb_Data.csv")
+View(Airbnb_Data)
+
+##Declaracion de variables
+
+Precio = Airbnb_Data$log_price
+TipoProp = Airbnb_Data$property_type
+TipoHab = Airbnb_Data$room_type
+Baños = Airbnb_Data$bathrooms
+TipoCama = Airbnb_Data$bed_type
+Cancelacion = Airbnb_Data$cancellation_policy
+Limpieza = Airbnb_Data$cleaning_fee
+Ciudad = Airbnb_Data$city
+Verificado = Airbnb_Data$host_identity_verified
+InstReserva = Airbnb_Data$instant_bookable
+Barrio = Airbnb_Data$neighbourhood
+NumReseña = Airbnb_Data$number_of_reviews
+Puntuacion = Airbnb_Data$review_scores_rating
+NumHabitaciones = Airbnb_Data$bedrooms
+NumCamas = Airbnb_Data$beds
+
+
+
+
+## Cambio log_price a precio normal
+
+Airbnb_Data$log_price = exp(Airbnb_Data$log_price)
+
+
+##
+plot(x = Airbnb_Data$log_price, main = "Grafica", xlab = "Precio", ylab = "Algo", col = c("royalblue"))
+
