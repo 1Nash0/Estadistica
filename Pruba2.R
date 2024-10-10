8377b714f013c7cccfbe1ad981d5a6e50bc934f2
César Alburquerque López <c.alburquerque.2023@alumnos.urjc.es>
2024-10-10 11:25:43 +0200

    Analisis Univariante casi terminado

--- Pruba2.R
+++ Pruba2.R
@@ -5,27 +5,32 @@ library(dplyr)
 library(readr)
 library(tidyverse)
 library(ggplot2)
+library(summarytools)
+library(GGally)
+library(knitr)
+library(ggcorrplot)
+library(htmltools)
 
 lab <- read_csv("~/ESTADISTICA/Practica Grupal/AnalisisAIRBNB/Airbnb_Data.csv")
 View(Airbnb_Data)
 
 ##Declaracion de variables
 
-Precio = Airbnb_Data$log_price
-TipoProp = Airbnb_Data$property_type
-TipoHab = Airbnb_Data$room_type
-Baños = Airbnb_Data$bathrooms
-TipoCama = Airbnb_Data$bed_type
-Cancelacion = Airbnb_Data$cancellation_policy
-Limpieza = Airbnb_Data$cleaning_fee
-Ciudad = Airbnb_Data$city
-Verificado = Airbnb_Data$host_identity_verified
-InstReserva = Airbnb_Data$instant_bookable
-Barrio = Airbnb_Data$neighbourhood
-NumReseña = Airbnb_Data$number_of_reviews
-Puntuacion = Airbnb_Data$review_scores_rating
-NumHabitaciones = Airbnb_Data$bedrooms
-NumCamas = Airbnb_Data$beds
+##Precio <- Airbnb_Data$log_price
+##TipoProp = Airbnb_Data$property_type
+##TipoHab = Airbnb_Data$room_type
+##Baños = Airbnb_Data$bathrooms
+##TipoCama = Airbnb_Data$bed_type
+##Cancelacion = Airbnb_Data$cancellation_policy
+##Limpieza = Airbnb_Data$cleaning_fee
+##Ciudad = Airbnb_Data$city
+##Verificado = Airbnb_Data$host_identity_verified
+##InstReserva = Airbnb_Data$instant_bookable
+##Barrio = Airbnb_Data$neighbourhood
+##NumReseña = Airbnb_Data$number_of_reviews
+##Puntuacion = Airbnb_Data$review_scores_rating
+##NumHabitaciones = Airbnb_Data$bedrooms
+##NumCamas = Airbnb_Data$beds
 
 
 
@@ -35,6 +40,21 @@ NumCamas = Airbnb_Data$beds
 Airbnb_Data$log_price = exp(Airbnb_Data$log_price)
 
 
-##
-plot(x = Airbnb_Data$log_price, main = "Grafica", xlab = "Precio", ylab = "Algo", col = c("royalblue"))
+num_respuestas_PRICE<- length(unique(Airbnb_Data$log_price))
+cat("La cantidad de respuestas obtenidas es:", num_respuestas_PRICE)
+
+## La cantidad de respuestas obtenidas es: 767
+
+##Como podemos observar, no tiene sentido realizar un estudio de esta variable continua.
+
+
+
+##HISTIOGRAMA DE PRECIOS
+
+hist(Airbnb_Data$log_price, col = "#CB0019", main = "Histograma de Precio", xlab = "log_price", ylab = "Frecuencia", ylim = c(0, 35000), xlim = c(0,1000), border = "black")
+
+##RESUMEN DE PRECIOS
+
+Airbnb_Data |> select(log_price) |> descr() |> kable(digits = 2)
+
 
